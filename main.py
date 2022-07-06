caa5173fe968a5acf19cb9c32732eeb65e740573
Sidhant Kumar Agrawal <sidhant21495@iiitd.ac.in>
2022-07-06 22:14:20 +0530

    Stage 1 final (few error handling remains)

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ output=[]
 
 def Input(instrn):
     instructions=-1
+    Flag=True
     while True:
         line = input()
         count =1
@@ -14,25 +15,35 @@ def Input(instrn):
             break
         line =line.split()
         # print(line)
-        if line[0]=='var':
+        if line[0]=='var' and Flag==True:
             if line[1] not in variables:
                 variables[line[1]]=count
                 count+=1
             else:
                 print("ERROR : Declaration of variable multiple times")
+                break
+        elif line[0]=='var' and Flag==False:
+            print("ERROR: Declaration of variable between the code")
+            break
+
         elif line[0].endswith(":"):
-            if (line[0]-":") in variables:
+            line[0]=line[0].replace(":","")
+            if (line[0]) in variables:
                 print("ERROR: Naming of label and variable is same")
-            elif (line[0]-":") in labels:
+                break
+            elif (line[0]) in labels:
                 print("ERROR : Use of label multiple times")
+                break
             else:
-                labels[line[0]-":"]=instructions
+                labels[line[0]]=instructions
                 line=line[1:]
                 instrn.append(line)
                 instructions+=1
+            Flag=False
         else:
             instrn.append(line)
             instructions+=1
+            Flag=False
     return instructions
 
 def Running(instrn,output,instructions):
@@ -51,16 +62,24 @@ def Running(instrn,output,instructions):
             TypeE(line,output,instructions)
         elif instrnType[line[0]]=='F':
             TypeF(line,output)
+        else:
+            print("ERROR: Invalid Opcode instruction")
         
 def Printing(output):
     for out in output:
         print(out)
         
 
-instructions=Input(instrn)
 
-Running(instrn,output,instructions)
-Printing(output)
+
+try:
+    instructions=Input(instrn)
+
+    Running(instrn,output,instructions)
+    Printing(output)
+
+except:
+     print("Something went Wrong")
 
 
 
