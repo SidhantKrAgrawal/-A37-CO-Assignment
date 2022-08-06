f4273a66a1dd4ce9b95901b2a3818338135685f1
Sidhant Kumar Agrawal <sidhant21495@iiitd.ac.in>
2022-08-06 08:26:13 +0530

    commit

--- Simulator/functions.py
+++ Simulator/functions.py
@@ -4,10 +4,11 @@ from dictionaries import *
 
 def TypeA(inst,reg1,reg2,reg3):
     mapping["PC"]+=1
+    mapping["flags"]=0
     x=registers[reg1]
     y=registers[reg2]
     z=registers[reg3]
-
+   
     if (inst=="add"):
         mapping[z]=mapping[y]+mapping[x]
     elif(inst=="sub"):
@@ -27,7 +28,7 @@ def TypeA(inst,reg1,reg2,reg3):
 
 def TypeB(inst,reg,imm):
     mapping["PC"]+=1
-    
+    mapping["flags"]=0
     x=registers[reg]
     
     if (inst=="movB"):
@@ -42,38 +43,63 @@ def TypeB(inst,reg,imm):
 
 def TypeC(inst,reg1,reg2):
     mapping["PC"]+=1
-
+    mapping["flags"]=0
     x=registers[reg1]
     y=registers[reg2]
 
     if(inst=="movC"):
-        pass
+        mapping[y]=mapping[x]
     elif(inst=="div"):
-        pass
+        pass              # Doubt
     elif(inst=="not"):
-        pass
+        mapping[y]= ~mapping[x]
     elif(inst=="cmp"):
-        if(mapping[x]<mapping[y]):
+        if mapping[x]<mapping[y]:
             mapping["flags"]=4
-        pass
+        elif mapping[x]>mapping[y]:
+            mapping["flags"]=2
+        elif mapping[x]==mapping[y]:
+            mapping["flags"]=1
 
     printing()
     return 0
 
 def TypeD(inst,reg1,mem_addr):
     mapping["PC"]+=1
-
+    mapping["flags"]=0
     x=registers[reg1]
 
     if(inst=="ld"):
         mem=int(mem_addr,2)
         mapping[x]=int(memory[mem],2)
+    elif(inst=='st'):
+        mem=int(mem_addr,2)
+        memory[mem]=DecToBin(mapping[x],16)
     
     printing()
     return 0
 
 def TypeE(inst,mem_addr):
-    pass
+    
+    if(inst=='jmp'):
+        mem=int(mem_addr,2)
+        mapping["PC"]=mem
+    elif(inst=='jlt'):
+        if(mapping["flags"]==4):
+            mem=int(mem_addr,2)
+            mapping["PC"]=mem
+    elif(inst=='jgt'):
+        if(mapping["flags"]==2):
+            mem=int(mem_addr,2)
+            mapping["PC"]=mem
+    elif(inst=='je'):
+        if(mapping["flags"]==1):
+            mem=int(mem_addr,2)
+            mapping["PC"]=mem
+
+    printing()
+    mapping["flags"]=0
+    return 0
 
 def TypeF(inst):
     pass
